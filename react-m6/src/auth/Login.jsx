027c9af6c745914be3d6ffc1e136e5974a41a907
judit maria <jumoar@fp.insjoaquimmir.cat>
2023-01-24 18:00:49 +0100

    Login/Register creado

--- react-m6/src/auth/Login.jsx
+++ react-m6/src/auth/Login.jsx
@@ -3,13 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Login = () => {
   return (
-    <div className='position-absolute top-50 start-50 translate-middle'>
-      <h1 className='fw-bold text-center'>Log in</h1>
-      <input type="text" class="form-control rounded-4" placeholder="Email or username" aria-label="Email or username" aria-describedby="Put your email or username"></input>
-      <div class="input-group mb-3">
-        <input type="text" class="form-control rounded-5 rounded-top-0" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-    </div>
+    <div>
+      <div className='position-absolute top-50 start-50 translate-middle d-grid gap-2 col-6 mx-auto'>
+        <h1 className='fw-bold text-center'>Log in</h1>
+        <input type="text" class="form-control" placeholder="Email or username" aria-label="Email or username" aria-describedby="Put your email or username" required/>
+        <div className="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="Put your password" required/>
+          <button className="btn btn-outline-secondary text-uppercase" type="button" id="button-addon2">forgot?</button>
+        </div>
+        <button className="btn btn-primary text-uppercase fw-bold" type="button">login</button>
+        <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              <hr/>
+            </div>
+            <div class="col">
+              <p className="text-center text-uppercase text-secondary">or</p>
+            </div>
+            <div class="col">
+              <hr/>
+            </div>
+          </div>
+        </div>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col d-grid">
+              <button className="btn btn-outline-primary text-uppercase" type="button">github</button>
+            </div>
+            <div className="col d-grid">
+              <button className="btn btn-outline-primary text-uppercase" type="button">google</button>
+            </div>
+          </div>
+        </div>
+        <p className="text-center">By signing in to ****, you agree to our Terms and Privacy Policy</p>
+        <p className="text-center text-decoration-underline text-secondary">Not registered ?</p>
+      </div>
     </div>
   );
 }
