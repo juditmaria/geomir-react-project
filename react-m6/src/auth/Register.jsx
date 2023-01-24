217f38bee89c11dd11210f1289b38a4783bf5abc
judit maria <jumoar@fp.insjoaquimmir.cat>
2023-01-24 19:16:30 +0100

    Retoque Login/Register

--- react-m6/src/auth/Register.jsx
+++ react-m6/src/auth/Register.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div className="">
-      <div className='position-absolute top-50 start-50 translate-middle d-grid gap-2 col-6 mx-auto'>
-        <h1 className='fw-bold text-center'>Create your profile</h1>
-        <input type="text" class="form-control" placeholder="Age" aria-label="Age" aria-describedby="Put your age" required/>
-        <input type="text" class="form-control" placeholder="Name (optional)" aria-label="Name (optional)" aria-describedby="Put your name (optional)"/>
-        <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="Put your email" required/>
-        <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="Put your password" required/>
-        <button className="btn btn-primary text-uppercase fw-bold" type="button">create account</button>
-        <p className="text-center text-decoration-underline text-secondary">Already registered ?</p>
-      </div>
+    <div className='position-absolute top-50 start-50 translate-middle d-grid gap-2 col-6 mx-auto'>
+      <h1 className='fw-bold text-center'>Create your profile</h1>
+      <input type="text" class="form-control" placeholder="Age" aria-label="Age" aria-describedby="Put your age" required/>
+      <input type="text" class="form-control" placeholder="Name (optional)" aria-label="Name (optional)" aria-describedby="Put your name (optional)"/>
+      <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="Put your email" required/>
+      <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="Put your password" required/>
+      <button className="btn btn-primary text-uppercase fw-bold" type="button">create account</button>
+      <p className="text-center text-decoration-underline text-secondary">Already registered ?</p>
     </div>
   )
 }
