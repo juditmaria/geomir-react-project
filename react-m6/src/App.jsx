f6bc00b8e80f2bc906914b2ae7897952119b2315
judit maria <jumoar@fp.insjoaquimmir.cat>
2023-01-27 19:18:31 +0100

    2.4 Oucltando/Mostrando componentes

--- react-m6/src/App.jsx
+++ react-m6/src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
 
 import { Login } from './auth/Login'
 import { Register } from './auth/Register'
 
 function App() {
 
+  let [authPage, setAuthPage] = useState(true);
+  
+/*   let [authPageController, setAuthPage] = useState(0);
+  let authPageDefault = 0;
+
+  const authPageTrue = () => {
+    setAuthPage(authPageController + 1);
+  };
+  const authPageFalse = () => {
+    authPageDefault--;
+
+    // Per a veure que es modifica
+    console.log(authPageDefault);
+  }; */
+
   return (
     <div className="App">
-      <Login />
-      {/* <Register /> */}
+      <button
+        onClick={() => {
+          setAuthPage(!authPage);
+        }}
+      >
+        Auth Page seleccioner
+      </button>
+      {authPage ? <Login /> : <Register />}
     </div>
   )
 }
--- react-m6/src/auth/Login.jsx
+++ react-m6/src/auth/Login.jsx
@@ -5,21 +5,21 @@ export const Login = () => {
   return (
     <div className='position-absolute top-50 start-50 translate-middle d-grid gap-2 col-6 mx-auto'>
       <h1 className='fw-bold text-center'>Log in</h1>
-      <input type="text" class="form-control" placeholder="Email or username" aria-label="Email or username" aria-describedby="Put your email or username" required/>
+      <input type="text" className="form-control" placeholder="Email or username" aria-label="Email or username" aria-describedby="Put your email or username" required/>
       <div className="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="Put your password" required/>
+        <input type="text" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="Put your password" required/>
         <button className="btn btn-outline-secondary text-uppercase" type="button" id="button-addon2">forgot?</button>
       </div>
       <button className="btn btn-primary text-uppercase fw-bold" type="button">login</button>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
             <hr/>
           </div>
-          <div class="col">
+          <div className="col">
             <p className="text-center text-uppercase text-secondary">or</p>
           </div>
-          <div class="col">
+          <div className="col">
             <hr/>
           </div>
         </div>
@@ -39,5 +39,3 @@ export const Login = () => {
     </div>
   );
 }
-
-
